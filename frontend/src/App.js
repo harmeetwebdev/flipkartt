612d2ab75e7b1f8a912a85f88be10d9c9a392ca7
Harmeet Bansal <harmeets627@gmail.com>
2022-11-29 22:07:58 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>flipkart
+          <br />
+          <strong>plus<span>+</span> </strong>
+        </a>
+      </header>
+      <main>
+        <h1>list of products</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;

2a6fb577c879821d299e7299f656376712b1a23a
Harmeet Bansal <harmeets627@gmail.com>
2022-11-29 23:12:10 +0530

    videi-create-flipkart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -11,7 +12,21 @@ function App() {
         </a>
       </header>
       <main>
-        <h1>list of products</h1>
+        <h1>Featured product</h1>
+        <div className='products'>
+          {data.products.map(product => (
+            <div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>)
+          )}
+        </div>
       </main>
     </div>
   );

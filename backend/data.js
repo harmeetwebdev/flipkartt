790d8f806b1ac7837dfaf0e8f02aba7152743fa9
Harmeet Bansal <harmeets627@gmail.com>
2022-11-30 14:19:51 +0530

    video-server.js

---
+++ backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+    products: [
+        {
+            name: "Nike Men shoes",
+            slug: "nike-men-shoes",
+            category: 'Footwear',
+            image: "/images/p1.jpg",
+            price: 2999,
+            countInStock: 45,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 50,
+            description: 'high quality shoes',
+
+        },
+        {
+            name: "Nike Women shoes",
+            slug: "nike-women-shoes",
+            category: 'Footwear',
+            image: "/images/p2.jpg",
+            price: 2456,
+            countInStock: 45,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 56,
+            description: 'high quality shoes',
+
+        },
+        {
+            name: "Addidas Men shoes",
+            slug: "addidas-men-shoes",
+            category: 'Footwear',
+            image: "/images/p3.jpg",
+            price: 2050,
+            countInStock: 30,
+            brand: 'Addidas',
+            rating: 4.9,
+            numReviews: 58,
+            description: 'high quality shoes',
+
+        },
+        {
+            name: "Addidas Women shoes",
+            slug: "addidas-women-shoes",
+            category: 'Footwear',
+            image: "/images/p4.jpg",
+            price: 3999,
+            countInStock: 60,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 20,
+            description: 'high quality shoes',
+
+        },
+    ]
+}
+export default data

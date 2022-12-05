151d209ca2f6d6469d2794b643a87b7064e3fa99
Harmeet Bansal <harmeets627@gmail.com>
2022-12-05 22:04:56 +0530

    video-cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,40 @@
 
-import { Container } from 'react-bootstrap';
+import { useContext } from 'react';
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import { Link } from 'react-router-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screen/HomeScreen ';
 import ProductScreen from './screen/ProductScreen';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className='d-flex  flex-column site-container'>
         <header>
-          <Link to="/">
-            flipkart
-            <br />
-            <strong>plus<span>+</span> </strong>
-          </Link>
+          <Navbar>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>flipkart</Navbar.Brand>
+              </LinkContainer>
+              <Nav className='me-auto'>
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+
         </header>
         <main>
           <Container className='mt-4  '>
--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react'
 import { Badge, Button, Card, Col, ListGroup, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom'
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
+import { Store } from '../Store';
 import { getError } from '../utils';
 
 const reducer = (state, action) => {
@@ -45,6 +46,13 @@ export default function ProductScreen() {
         }
         fetchData()
     }, [slug]);
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const addToCartHandler = () => {
+        ctxDispatch({
+            type: "CART_ADD_ITEM",
+            payload: { ...product, quantity: 1 }
+        })
+    }
     return (
         loading ? (
             <LoadingBox />
@@ -105,7 +113,9 @@ export default function ProductScreen() {
                                 {product.countInStock > 0 && (
                                     <ListGroup.Item>
                                         <div className='d-grid'>
-                                            <Button variant="outline-warning">Add To Cart</Button>
+                                            <Button
+                                                onClick={addToCartHandler}
+                                                variant="outline-warning">Add To Cart</Button>
                                         </div>
                                     </ListGroup.Item>
                                 )}
